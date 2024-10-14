0c94c6d49380a10d08668bf1da8606c2cb6a83de
Savenok <Romasav2017@gmail.com>
2024-10-14 20:01:56 +0300

    4.0 14.10.24

--- pr240823/assets/Js/main.js
+++ pr240823/assets/Js/main.js
@@ -109,3 +109,20 @@ imgElm.forEach(el => {
 
 })
 
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+
+        }
+    });
+}
+let options = {
+    threshold: [0.10]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
